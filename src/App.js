3dfb79580d75af0e53e5b870911ef0458cf6bbec
Janardhan-B <73274827+J-A-N-A@users.noreply.github.com>
2022-09-20 21:59:04 +0530

    Add files via upload
    2nd commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navi from "./components/A-Navbar.top";
+import Body from "./components/B-Body.topmid";
+import Body2 from "./components/C.Body.mid";
+import Foo from "./components/Foo";
+import SpeedDialbasic from "./components/SpeedDial";
+import './App.css'
+function App() {
+  return (
+    <>
+      <Navi />
+      <Body />  
+      <Body2 />
+      <Foo />
+
+    </> 
+  );
+}
+
+export default App;
